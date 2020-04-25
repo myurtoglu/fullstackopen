3275513008f4f9957ca2e096f344284dd93090da
Mete Yurtoglu <myurtoglu@gmail.com>
2020-04-25 10:40:28 -0700

    Complete step1 of phonebook.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,24 +1,40 @@
 import React, { useState } from 'react'
 
+const Numbers = ({ persons }) => {
+  return persons.map(
+    person => <div key={person.name}>{person.name}</div>)
+}
+
 const App = () => {
   const [ persons, setPersons ] = useState([
     { name: 'Arto Hellas' }
   ]) 
   const [ newName, setNewName ] = useState('')
 
+  const handleNoteChange = (event) => setNewName(event.target.value)
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const newPerson = {
+      name: newName,
+    }
+    setPersons(persons.concat(newPerson))
+    setNewName("")
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNoteChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <Numbers persons={persons}/>
     </div>
   )
 }
