92f49a8c325192b60ac2783192615def800ac102
Mete Yurtoglu <myurtoglu@gmail.com>
2020-04-25 11:08:35 -0700

    Complete step4 of phonebook.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,28 @@
 import React, { useState } from 'react'
 
-const Numbers = ({ persons }) => {
-  return persons.map(
-    person => <div key={person.name}>{person.name} {person.number}</div>)
+const Numbers = ({ persons, searchQuery }) => {
+  return persons
+    .filter(
+      person => person.name.toLowerCase().includes(searchQuery.toLowerCase()))
+    .map(
+      person => <div key={person.name}>{person.name} {person.number}</div>)
 }
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: "1"}
-  ]) 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ searchQuery, setSearchQuery ] = useState('')
 
   const handleNameChange = (event) => setNewName(event.target.value)
   const handleNumberChange = (event) => setNewNumber(event.target.value)
+  const handleSearchQueryChange = (event) => setSearchQuery(event.target.value)
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -33,6 +42,8 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      filter shown with <input value={searchQuery} onChange={handleSearchQueryChange} />
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div> name: <input value={newName} onChange={handleNameChange} /> </div>
         <div> number: <input value={newNumber} onChange={handleNumberChange} /> </div>
@@ -41,7 +52,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <Numbers persons={persons}/>
+      <Numbers persons={persons} searchQuery={searchQuery}/>
     </div>
   )
 }
