fe128c2b17e6be3cc073b3efb3d667f58fb42aac
Mete Yurtoglu <myurtoglu@gmail.com>
2020-05-16 21:37:07 -0700

    Complete 4.4.

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,11 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogPosts) => {
+  return blogPosts.reduce((total, current) => total + current.likes, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
