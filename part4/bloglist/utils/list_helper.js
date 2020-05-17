1c3965c8cfa917f638b9a9fdab49e2c8a07a6083
Mete Yurtoglu <myurtoglu@gmail.com>
2020-05-16 22:18:51 -0700

    Complete 4.6 & 4.7.

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -18,8 +18,46 @@ const favoriteBlog = (blogPosts) => {
   }
 }
 
+const mostBlogs = (blogPosts) => {
+  if (blogPosts.length === 0) { return {} }
+  let authorsWithPostCounts = {}
+  blogPosts.forEach(post => {
+    const author = post.author
+    authorsWithPostCounts[author] = Object.prototype.hasOwnProperty.call(
+      authorsWithPostCounts, author) ? authorsWithPostCounts[author] + 1 : 1
+  })
+  const maxPostCount = Math.max(...Object.values(authorsWithPostCounts))
+  let topEntry = Object.entries(authorsWithPostCounts).find(([, posts]) => {
+    return posts === maxPostCount
+  })
+  return {
+    author: topEntry[0],
+    blogs: topEntry[1]
+  }
+}
+
+const mostLikes = (blogPosts) => {
+  if (blogPosts.length === 0) { return {} }
+  let authorsWithLikeCounts = {}
+  blogPosts.forEach(post => {
+    const author = post.author
+    authorsWithLikeCounts[author] = Object.prototype.hasOwnProperty.call(
+      authorsWithLikeCounts, author) ? authorsWithLikeCounts[author] + post.likes : post.likes
+  })
+  const maxLikeCount = Math.max(...Object.values(authorsWithLikeCounts))
+  let topEntry = Object.entries(authorsWithLikeCounts).find(([, posts]) => {
+    return posts === maxLikeCount
+  })
+  return {
+    author: topEntry[0],
+    likes: topEntry[1]
+  }
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
 }
