1b360b55db02bd46c5aa28496df411073bf92060
Mete Yurtoglu <myurtoglu@gmail.com>
2020-05-16 21:50:45 -0700

    Complete 4.5.

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,7 +6,20 @@ const totalLikes = (blogPosts) => {
   return blogPosts.reduce((total, current) => total + current.likes, 0)
 }
 
+const favoriteBlog = (blogPosts) => {
+  if (blogPosts.length === 0) { return {} }
+
+  const maxLikes = Math.max(...blogPosts.map(post => post.likes))
+  const mostLikedPost = blogPosts.find(post => post.likes === maxLikes)
+  return {
+    title: mostLikedPost.title,
+    author: mostLikedPost.author,
+    likes: mostLikedPost.likes
+  }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
