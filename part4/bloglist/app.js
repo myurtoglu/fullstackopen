360f4aa3739b6874b88b7fb586026d0084913560
Mete Yurtoglu <myurtoglu@gmail.com>
2020-05-26 22:35:24 -0700

    Complete 4.18 & 4.19.

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -5,6 +5,7 @@ const config = require('./utils/config')
 const cors = require('cors')
 const blogRouter = require('./controllers/blogrouter')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const mongoose = require('mongoose')
 const middleware = require('./utils/middleware')
 
@@ -19,6 +20,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
